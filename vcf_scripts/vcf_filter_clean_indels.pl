bb23092ed54fc8dc9adb1ee37b0d207dd41fa810
Isaac Turner <turner.isaac@gmail.com>
2012-01-11 19:53:58 +0000

    Added --invert option

--- vcf_scripts/vcf_filter_clean_indels.pl
+++ vcf_scripts/vcf_filter_clean_indels.pl
@@ -12,13 +12,21 @@ sub print_usage
     print STDERR "Error: $err\n";
   }
 
-  print STDERR "Usage: ./vcf_filter_clean_indels.pl [in.vcf]\n";
+  print STDERR "Usage: ./vcf_filter_clean_indels.pl [--invert] [in.vcf]\n";
   print STDERR "  Prints variants were one allele is 0bp and the other is >0bp\n";
   exit;
 }
 
-if(@ARGV > 1 || @ARGV == 1 && $ARGV[0] =~ /-?-h(elp)?/i) {
+if(@ARGV > 2)
+{
   print_usage();
+}
+
+my $invert = 0;
+
+if(@ARGV > 0 && $ARGV[0] =~ /^-?-invert?/i)
+{
+  $invert = 1;
 }
 
 my $vcf_file = shift;
@@ -48,7 +56,7 @@ my $vcf = new VCFFile($vcf_handle);
 print $vcf->get_header();
 
 my $num_of_variants = 0;
-my $num_of_clean = 0;
+my $num_printed = 0;
 
 my $vcf_entry;
 
@@ -56,20 +64,22 @@ while(defined($vcf_entry = $vcf->read_entry()))
 {
   $num_of_variants++;
 
-  if($vcf_entry->{'INFO'}->{'SVLEN'} > 0 &&
-     (length($vcf_entry->{'true_REF'}) == 0 ||
-      length($vcf_entry->{'true_ALT'}) == 0))
+  my $is_clean = ($vcf_entry->{'INFO'}->{'SVLEN'} > 0 &&
+                  (length($vcf_entry->{'true_REF'}) == 0 ||
+                   length($vcf_entry->{'true_ALT'}) == 0));
+
+  if($is_clean != $invert)
   {
-    $num_of_clean++;
+    $num_printed++;
     $vcf->print_entry($vcf_entry);
   }
 }
 
 # Print filtered rate
-my $printed_percent = 100 * $num_of_clean / $num_of_variants;
+my $printed_percent = 100 * $num_printed / $num_of_variants;
 
 print STDERR "vcf_get_clean_indels.pl: " .
-             num2str($num_of_clean) . " / " . num2str($num_of_variants) . " " .
+             num2str($num_printed) . " / " . num2str($num_of_variants) . " " .
              "(" . sprintf("%.2f", $printed_percent) . "%) variants printed\n";
 
 close($vcf_handle);
