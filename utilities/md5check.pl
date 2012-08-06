3c9689731ac9eddee6b1a13d7ffcec16716c92f6
Isaac Turner <turner.isaac@gmail.com>
2012-08-06 21:56:55 +0100

    Consistent md5check output.

--- utilities/md5check.pl
+++ utilities/md5check.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use List::Util qw(reduce);
+
 sub print_usage
 {
   for my $err (@_) {
@@ -11,7 +13,18 @@ sub print_usage
 
   print STDERR "" .
 "usage: ./md5check.pl <file1 ..>
-  Compares md5sum output\n";
+  Compares multiple md5sum output lists
+
+  Example output:
+    06ba2ae0897750fe14ac0b8b51b8cd4e  file1.txt.gz [2]
+    Error: file2.txt.gz [2]
+      60b725f10c9c85c70d97880dfe8191b3 [md5sums1.txt]
+      b026324c6904b2a9cb4b88d6d61c81d1 [md5sums2.txt]
+    26ab0db90d72e28ad0ba1e22ee510510  file3.txt.gz [2]
+
+  Note: The number of hash entries for each file is shown in [x].
+        If there is a mismatch between hashes, all hashes and their sources
+        are shown.\n";
 }
 
 if(@ARGV < 1)
@@ -56,18 +69,20 @@ for my $file (@files)
 {
   my @hashes_for_file = keys %{$file_hashes{$file}};
 
+  my $num_of_entries = 0;
+  map {$num_of_entries += scalar(@{$file_hashes{$file}->{$_}})} @hashes_for_file;
+
   if(@hashes_for_file > 1)
   {
-    print "Error: " . $file . " [".scalar(@hashes_for_file)."]\n";
+    print "Error: " . $file . " [$num_of_entries]\n";
     for my $hash (@hashes_for_file)
     {
-      print "  $hash [".join(" ", @{$file_hashes{$file}->{$hash}})."]\n";
+      print "  $hash [" . join(" ", @{$file_hashes{$file}->{$hash}}) . "]\n";
     }
   }
   else
   {
     # File only has one hash - show the number of md5sum files that list it
-    print $hashes_for_file[0] . "  " . $file . " [" .
-          scalar(@{$file_hashes{$file}->{$hashes_for_file[0]}}) . "]\n";
+    print "$hashes_for_file[0]  $file [$num_of_entries]\n";
   }
 }
