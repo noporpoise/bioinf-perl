8239c422e26f3d63c92cc1c6122fb47645a49483
Isaac Turner <turner.isaac@gmail.com>
2012-11-30 19:27:02 +0000

    More accurate memory estimation. Added paths option

--- cortex_scripts/cortex_memory.pl
+++ cortex_scripts/cortex_memory.pl
@@ -26,6 +26,7 @@ sub usage
   print "Usage: ./cortex_memory.pl [options] <mem_height> <mem_width>\n";
   print "  Options: --kmer_size -k <k>   K-mer size of De Bruijn graph [default: 31]\n";
   print "           --colours -c <num>   Number of colours in graph    [default: 1]\n";
+  print "           --paths -p <num>     Number of paths in graph      [default: 0]\n";
   exit;
 }
 
@@ -36,13 +37,14 @@ if(@ARGV < 2 || @ARGV > 6 || @ARGV % 2 != 0) {
 my $memHeight = $ARGV[@ARGV-2];
 my $memWidth = $ARGV[@ARGV-1];
 
-my $kmerSize = undef;
-my $numOfColours = undef;
+my $kmerSize = 31;
+my $numOfColours = 1;
+my $numOfPaths = 0;
 
 # Loop through options
 for(my $i = 0; $i < @ARGV-2; $i+=2)
 {
-  if($ARGV[$i] =~ m/^\-\-kmer\_size$/i || $ARGV[$i] =~ m/^\-k$/i)
+  if($ARGV[$i] =~ /^-?-k(mer(_?size)?)?$/i)
   {
     $kmerSize = $ARGV[$i+1];
 
@@ -50,12 +52,27 @@ for(my $i = 0; $i < @ARGV-2; $i+=2)
       usage("invalid kmer");
     }
   }
-  elsif($ARGV[$i] =~ m/^\-\-colours$/i || $ARGV[$i] =~ m/^\-c$/i)
+  elsif($ARGV[$i] =~ /^-?-c(olours?)?$/i)
   {
     $numOfColours = $ARGV[$i+1];
     
     if($numOfColours !~ /^[0-9]+$/ || $numOfColours == 0) {
       usage("invalid number of colours");
+    }
+  }
+  elsif($ARGV[$i] =~ /^-?-p(aths?)?$/i)
+  {
+    $numOfPaths = $ARGV[$i+1];
+    
+    if($numOfPaths !~ /^[0-9]+$/) {
+      usage("invalid number of colours");
+    }
+
+    if($numOfPaths % 8 != 0)
+    {
+      print STDERR "Warning: paths is usually a multiple of 8 or 0\n";
+      # Round up to nearest eight
+      $numOfPaths = ceil($numOfPaths / 8) * 8;
     }
   }
   else
@@ -64,13 +81,7 @@ for(my $i = 0; $i < @ARGV-2; $i+=2)
   }
 }
 
-if(!defined($kmerSize)) {
-  $kmerSize = 31;
-}
-
-if(!defined($numOfColours)) {
-  $numOfColours = 1;
-}
+my $numOfPathBytes = $numOfPaths / 8;
 
 if($memHeight !~ /^[0-9]+$/ || $memHeight < 1) {
   usage("invalid memHeight");
@@ -90,8 +101,18 @@ print "Buckets: " . num2str($num_of_buckets) . "\n";
 print "Hashtable entries: " . num2str($num_of_hash_entries) . "\n";
 
 # Round entry size to nearest 8 bytes and multiply by the number of entries
-my $bytes = $num_of_hash_entries *
-            ceil((8*ceil($kmerSize/31) + 5*$numOfColours + 1)/8)*8;
+my $element_size = 8*ceil($kmerSize/32) + 5*$numOfColours + 1;
+
+# Add path bytes
+$element_size += $numOfColours*$numOfPathBytes;
+
+# Don't need to round up
+#$element_size = ceil($element_size / 8) * 8;
+
+my $bytes = $num_of_hash_entries * $element_size;
+
+# ~1GB used for keeping track of bucket usage
+$bytes += $num_of_buckets * 2;
 
 print "Memory: " . num2str($bytes) . " bytes (" . mem2str($bytes, 0, 1) . ")\n";
 
