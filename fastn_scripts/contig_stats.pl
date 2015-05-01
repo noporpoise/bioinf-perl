1d8ca11a4ae46e721388ff26f6811543ab48a90f
Isaac Turner <turner.isaac@gmail.com>
2015-05-01 15:00:34 +0100

    Report NG50

--- fastn_scripts/contig_stats.pl
+++ fastn_scripts/contig_stats.pl
@@ -18,25 +18,30 @@ sub print_usage
   for my $err (@_) { print STDERR "Error: $err\n"; }
 
   print STDERR "" .
-"Usage: ./contig_stats.pl [--print-csv] <in.fa|fq>
+"Usage: ./contig_stats.pl [options] <in.fa|fq>
   Length distribution stats.
+
+Options:
+  -c, --print-csv   Print output as CSV
+  -g, --genome <G>  calculate NG50 with genome size of <G> e.g. 3.1G
 ";
 
   exit(-1);
 }
 
 my $print_csv = 0;
+my $genome_size;
 
-while(@ARGV > 1) {
-  if($ARGV[0] =~ /^--print-csv$/) {
-    shift(@ARGV);
-    $print_csv = 1;
-  } else {
-    print_usage("Bad argument: $ARGV[0]");
-  }
+while(@ARGV > 1 && $ARGV[0] =~ /^-/) {
+  my $arg = shift;
+  if($arg =~ /^(-c|--print-csv)$/) { $print_csv = 1; }
+  elsif($arg =~ /^(-g|--genome)$/) { $genome_size = str2num(shift(@ARGV)); }
+  else { print_usage("Bad argument: $arg"); }
 }
 
 if(@ARGV != 1) { print_usage(); }
+
+print STDERR "Genome size: $genome_size\n";
 
 my $path = shift(@ARGV);
 my @lengths = ();
@@ -50,6 +55,7 @@ close_fastn_file($fastn);
 
 # min max median mode n50
 @lengths = sort{$a <=> $b} @lengths;
+if(@lengths > 1 && $lengths[0] < $lengths[1]) { die("Sort failed"); }
 my $ncontigs = scalar(@lengths);
 my $sum = sum(@lengths);
 
@@ -57,7 +63,8 @@ if($ncontigs == 0) { print STDERR "[contig_stats.pl] No sequences\n"; exit -1; }
 
 my $median = find_median(@lengths);
 my $mode = find_mode(@lengths);
-my $n50 = find_N50($sum,@lengths);
+my $n50  = find_N50($sum,$sum/2,@lengths);
+my $ng50 = defined($genome_size) ? find_N50($sum,$genome_size/2,@lengths) : 0;
 
 # Printing
 my $lwidth = 7;
@@ -74,6 +81,10 @@ print_cols("mean",    $lwidth, num2str($sum/$ncontigs,',',1,1), $rwidth+2);
 print_cols("median",  $lwidth, num2str($median,',',1,1),        $rwidth+2);
 print_cols("mode",    $lwidth, num2str($mode),                  $rwidth);
 print_cols("N50",     $lwidth, num2str($n50),                   $rwidth);
+
+if(defined($genome_size)) {
+  print_cols("NG50",  $lwidth, num2str($ng50),                  $rwidth);
+}
 
 sub print_cols
 {
@@ -123,12 +134,14 @@ sub find_median
   }
 }
 
-# usage: find_N50($total,@array)
-# @array must be sorted
+# usage: find_N50($total,$half,@array)
+#  returns the length which contigs of the same length or longer sum up to
+#  be >= $half.
+#  @array must be sorted longest to shortest
 sub find_N50
 {
-  my $total = shift;
-  my ($i, $sum, $half) = (0, 0, $total/2);
+  my ($total,$half) = @_;
+  my ($i, $sum) = (0, 0);
 
   if(@_ == 0 || $half == 0) { return undef; }
 
